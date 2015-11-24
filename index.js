9904c1c2a0465db41e1c7687b70844f771470eb2
Brian Beck <exogen@gmail.com>
2015-11-24 14:04:37 -0800

    Lower cache value from shields.io

--- index.js
+++ index.js
@@ -81,7 +81,11 @@ app.get("/:user/:repo", function (req, res) {
       failed: "red"
     }[status] || "lightgrey";
     badgeURL = getShieldURL(label, status, color);
-    request.get(badgeURL).pipe(res);
+    req.pipe(request.get(badgeURL)).on('response', function(res) {
+      res.headers['cache-control'] = "public, max-age=30";
+      delete res.headers['expires'];
+      delete res.headers['set-cookie'];
+    }).pipe(res);
   }
 });
 
