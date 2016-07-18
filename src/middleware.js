978425c95077c11682d9f01a378f0e2fd7513f63
Brian Beck <exogen@gmail.com>
2016-07-18 01:35:51 -0700

    Fix HEAD requests breaking SVG responses

--- src/middleware.js
+++ src/middleware.js
@@ -12,9 +12,13 @@ export function svgo(options) {
         return /image\/svg\+xml(;|$)/.test(res.get("content-type"));
       },
       intercept: function(body, send) {
-        svgo.optimize(body, (result) => {
-          send(result.data);
-        });
+        if (body) {
+          svgo.optimize(body, (result) => {
+            send(result.data);
+          });
+        } else {
+          send(body);
+        }
       }
     };
   });
